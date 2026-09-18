4497d72ea88c0e670f4179db45879492e656cc38
Ozan <ozan.arikan@hotmail.com>
2026-09-18 15:47:14 +0300

    Add Is Leap Year Go exercise and documentation

---
+++ Day 03 - Conditionals & Loops/07. isLeapYear.go
@@ -0,0 +1,14 @@
+package sprint
+
+func IsLeapYear(year int) bool {
+	if year%4 == 0 {
+		if year%100 == 0 {
+			if year%400 == 0 {
+				return true
+			}
+			return false
+		}
+		return true
+	}
+	return false
+}
