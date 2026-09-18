479c3df9ad72343665f826e8ad5b5d26ea4bf79e
Ozan <ozan.arikan@hotmail.com>
2026-09-18 16:30:30 +0300

    Add Countdown Go exercise

--- Day 05 - Going in Depth/05. countdown.go
+++ Day 05 - Going in Depth/05. countdown.go
@@ -3,13 +3,13 @@ package sprint
 func Countdown(n int) string {
 	x := ""
 	if n < 1 {
-	return "0!"
+		return "0!"
 	}
-	for i := n; i > 0; i -=2{
-	if x != ""{
-		x += ", "
-	}
-	x += string(rune(i + '0'))
+	for i := n; i > 0; i -= 2 {
+		if x != "" {
+			x += ", "
+		}
+		x += string(rune(i + '0'))
 	}
 	x += ", 0!"
 	return x
