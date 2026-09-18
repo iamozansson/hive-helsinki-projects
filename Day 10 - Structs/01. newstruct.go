2966555f6cdcb2235dc16083e3bb3c2b341a934b
Ozan <ozan.arikan@hotmail.com>
2026-09-18 19:32:29 +0300

    Add New Struct Go exercise and documentation

---
+++ Day 10 - Structs/01. newstruct.go
@@ -0,0 +1,16 @@
+package sprint
+
+type Point struct {
+	X    float32
+	Y    float32
+	Text string
+}
+
+func MakePoint(x, y float32, text string) Point {
+	var p Point = Point{
+		X:    x,
+		Y:    y,
+		Text: text,
+	}
+	return p
+}
